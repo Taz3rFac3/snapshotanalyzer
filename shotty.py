dbc77cb3632ed395caf79323b3ef92d11741c96d
robsinex <rob@securedoutcomes.com>
2020-05-21 11:15:53 -0400

    code to view AWS instances

--- shotty.py
+++ shotty.py
@@ -1,22 +1,56 @@
+from typing import List, Any
+
 import boto3
 import click
 
 session = boto3.Session(profile_name='shotty')
 ec2 = session.resource('ec2')
 
-@click.command()
-def list_instances():
+
+@click.group()
+def instances():
+    """Commands for Instances"""
+
+
+@instances.command('list')
+@click.option('--project', default=None,
+              help="Only instances for project (tag Project:<name>)")
+def list_instances(project):
     "List EC2 Instances"
-    for i in ec2.instances.all():
+    instances = []
+    if project:
+        filters = [{'Name': 'tag:Project', 'Values': [project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    for i in instances:
+        tags = {t['Key']: t['Value'] for t in i.tags or []}
         print(', '.join((
             i.id,
-            i.instances_type,
+            i.instance_type,
             i.placement['AvailabilityZone'],
             i.state['Name'],
-            i.public_dns_name)))
-        return
+            i.public_dns_name,
+            tags.get('Project', '<no project>'))))
+    return
+
+
+@instances.command('stop')
+@click.option('--project', default=None,
+              help='Only instances for project')
+def stop_intances(project):
+    "Stop EC2 instances"
+    instances = []
+    if project:
+        filters = [{'Name': 'tag:Project', 'Values': [project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+    for i in instances:
+        print("Stopping{0}...".format(i.id))
+        i.stop()
+    return
 
 if __name__ == '__main__':
-    list_instances()
-
-
+    instances()
